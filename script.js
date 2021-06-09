9bf3fa97a1b9aeb9fdecfa8f77a48d19b9f2f0a8
Artx999 <54852665+Artx999@users.noreply.github.com>
2021-06-10 01:22:51 +0200

    Added the feature of generating a visual board
    Also:
    - Fixed so that errors are more correct
    - Renamed a few variables
    - Made it so that the amount of bombs on the board is calculated separately to the board size.
    
    Notes:
    - The visual board needs a lot of visual as well as functional upgrades.

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ class Square {
         this.coords = coords
         this.isBomb = isBomb
     }
+    click() {
+        console.log(this.coords)
+    }
 }
 class Board {
     constructor() {
@@ -14,38 +17,56 @@ class Board {
     }
     /**
      * @param {int} difficulty
+     * @param {int} bombAmount
      */
-    create(difficulty) {
+    create(difficulty, bombAmount) {
         console.log("Creating board...")
-        // Checking if the difficulty is valid
-        if (!Number.isInteger(difficulty)) throw new Error("Difficulty not set to int")
+        // Checking if the difficulty and bombAmount is valid
+        if (!Number.isInteger(difficulty)) throw new Error("Difficulty not set to int or undefined")
         let minDifficulty = 0, maxDifficulty = 2
         if (difficulty < minDifficulty || difficulty > maxDifficulty) throw new Error("Difficulty must be between " + minDifficulty + " and " + maxDifficulty)
+        if (!Number.isInteger(bombAmount)) throw new Error("Amount of bombs not set to int or undefined")
 
+        // Defines board size
         this.difficulty = difficulty
-        let size = (this.difficulty + 1) * 5
-        let bombAmount = (this.difficulty + 1) * 10
+        let xSize = (this.difficulty + 1) * 5
+        let ySize = (this.difficulty + 1) * 5
 
         // Get bombs cords
         this.bombsCoords = []
         for (let i = 0; i < bombAmount; i++) {
-            let newBomb = [randomIntFromInterval(0, size), randomIntFromInterval(0, size)]
+            let newBomb = [randomIntFromInterval(0, xSize - 1), randomIntFromInterval(0, ySize - 1)]
             if (!JSON.stringify(this.bombsCoords).includes(JSON.stringify(newBomb))) this.bombsCoords.push(newBomb)
             else i--
         }
 
         // Generate squares
-        for (let x = 0; x < size; x++) {
-            for (let y = 0; y < size; y++) {
+        for (let x = 0; x < xSize; x++) {
+            for (let y = 0; y < ySize; y++) {
                 let coords = [x, y]
                 let isBomb = false
                 isBomb = JSON.stringify(this.bombsCoords).includes(JSON.stringify(coords));
                 this.squaresList.push(new Square(coords, isBomb))
             }
         }
+
+        // Generate the visual board
+        let board = $("#board")
+        board.css('--grid-rows', xSize);
+        board.css('--grid-cols', ySize);
+        this.squaresList.forEach(function (item) {
+            let square = $("<div></div>")
+                .addClass("square")
+                .html(JSON.stringify(item.coords))
+                .on("click", function () {
+                    item.click()
+                })
+            board.append(square)
+        })
         console.log("Created board")
     }
 }
+
 /*
 
 class Game {
@@ -63,10 +84,11 @@ game = new Game()
 game.start()
 
  */
+
 function randomIntFromInterval(min, max) { // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 let board = new Board()
-board.create(1)
+board.create(1, 2)
 console.log(board)
